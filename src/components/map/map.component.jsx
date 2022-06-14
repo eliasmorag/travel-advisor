5362482fc965cd08edcdffbd19dbd5f97f2c3902
Elías <eliasmora94@gmail.com>
2022-06-14 12:18:22 -0400

    Map structure and styling

--- src/components/map/map.component.jsx
+++ src/components/map/map.component.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useMediaQuery } from "@material-ui/core";
+import GoogleMapReact from "google-map-react";
 
-import "./map.styles";
+import useStyles from "./map.styles";
 
 const Map = () => {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+
+  // Set to false if larger than specified value
+  const isMobile = useMediaQuery("(min-width:600px)");
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
