9219b3084d0c94c1b61a22352dc954a78f21a629
Elías <eliasmora94@gmail.com>
2022-06-14 11:09:41 -0400

    Header stucture and styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { AppBar, Box, InputBase, Toolbar, Typography } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { Autocomplete } from "@react-google-maps/api";
 
-import "./header.styles";
+import useStyles from "./header.styles";
 
 const Header = () => {
-  return <h1>Header</h1>;
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore places
+          </Typography>
+          {/* <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
