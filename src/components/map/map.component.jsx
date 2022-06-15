a9baadb355c99ad35f7e5b37674e2d0473cf580b
Elías <eliasmora94@gmail.com>
2022-06-15 09:17:05 -0400

    Display Places on the Map

--- src/components/map/map.component.jsx
+++ src/components/map/map.component.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { useMediaQuery } from "@material-ui/core";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import GoogleMapReact from "google-map-react";
+import { Rating } from "@material-ui/lab";
 
 import useStyles from "./map.styles";
 
-const Map = ({ coordinates, setCoordinates, setBounds }) => {
+const Map = ({ coordinates, setCoordinates, setBounds, places }) => {
   const classes = useStyles();
 
   // Set to false if larger than specified value
-  const isMobile = useMediaQuery("(min-width:600px)");
+  const isDesktop = useMediaQuery("(min-width:600px)");
 
   return (
     <div className={classes.mapContainer}>
@@ -27,7 +29,49 @@ const Map = ({ coordinates, setCoordinates, setBounds }) => {
         onChildClick={(child) => {
           console.log(child);
         }}
-      ></GoogleMapReact>
+      >
+        {places.length &&
+          places
+            .filter((place) => place.latitude && place.longitude)
+            .map((place, i) => (
+              <div
+                className={classes.markerContainer}
+                lat={Number(place.latitude)}
+                lng={Number(place.longitude)}
+                key={i}
+              >
+                {!isDesktop ? (
+                  <LocationOnOutlinedIcon color="primary" fontSize="large" />
+                ) : (
+                  <Paper elevation={3} className={classes.paper}>
+                    <Typography
+                      className={classes.typography}
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      {" "}
+                      {place.name}
+                    </Typography>
+                    <img
+                      className={classes.pointer}
+                      src={
+                        place.photo
+                          ? place.photo.images.large.url
+                          : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                      }
+                      alt={place.name}
+                    />
+                    <Rating
+                      name="read-only"
+                      size="small"
+                      value={Number(place.rating)}
+                      readOnly
+                    />
+                  </Paper>
+                )}
+              </div>
+            ))}
+      </GoogleMapReact>
     </div>
   );
 };
